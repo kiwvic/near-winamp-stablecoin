75ac58ab91ddc4bcf1f73ead3d27cba858b59359
kiwvic <torogao@protonmail.com>
2022-07-21 17:25:03 +0300

    use near_contract_standards instead of explicit files

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,83 @@
-mod core;
-mod core_impl;
-mod receiver;
-mod resolver;
-mod events;
-mod event;
+use near_contract_standards::fungible_token::metadata::{
+    FungibleTokenMetadata, FungibleTokenMetadataProvider, FT_METADATA_SPEC,
+};
+use near_contract_standards::fungible_token::FungibleToken;
+use near_sdk::borsh::{self, BorshDeserialize, BorshSerialize};
+use near_sdk::collections::LazyOption;
+use near_sdk::json_types::U128;
+use near_sdk::{env, log, near_bindgen, AccountId, Balance, PanicOnDefault, PromiseOrValue};
+
+#[near_bindgen]
+#[derive(BorshDeserialize, BorshSerialize, PanicOnDefault)]
+pub struct Contract {
+    token: FungibleToken,
+    metadata: LazyOption<FungibleTokenMetadata>,
+}
+
+const SVG_PARAS_ICON: &str = "";
+const TOTAL_SUPPLY: Balance = 100_000_000_000_000_000_000_000_000;
+
+
+#[near_bindgen]
+impl Contract {
+    #[init]
+    pub fn new_paras_meta(owner_id: AccountId) -> Self {
+        Self::new(
+            owner_id,
+            U128(TOTAL_SUPPLY),
+            FungibleTokenMetadata {
+                spec: FT_METADATA_SPEC.to_string(),
+                name: "LAMA".to_string(),
+                symbol: "LAMA".to_string(),
+                icon: Some(SVG_PARAS_ICON.to_string()),
+                reference: None,
+                reference_hash: None,
+                decimals: 18
+            },
+        )
+    }
+
+    #[init]
+    pub fn new(
+        owner_id: AccountId,
+        total_supply: U128,
+        metadata: FungibleTokenMetadata,
+    ) -> Self {
+        assert!(!env::state_exists(), "Already initialized");
+
+        metadata.assert_valid();
+
+        let mut this = Self {
+            token: FungibleToken::new(b"a".to_vec()),
+            metadata: LazyOption::new(b"m".to_vec(), Some(&metadata)),
+        };
+        this.token.internal_register_account(&owner_id);
+        this.token.internal_deposit(&owner_id, total_supply.into());
+
+        near_contract_standards::fungible_token::events::FtMint {
+            owner_id: &owner_id,
+            amount: &total_supply,
+            memo: Some("Initial tokens supply is minted"),
+        }.emit();
+
+        this
+    }
+
+    fn on_account_closed(&mut self, account_id: AccountId, balance: Balance) {
+        log!("Closed @{} with {}", account_id, balance);
+    }
+
+    fn on_tokens_burned(&mut self, account_id: AccountId, amount: Balance) {
+        log!("Account @{} burned {}", account_id, amount);
+    }
+}
+
+near_contract_standards::impl_fungible_token_core!(Contract, token, on_tokens_burned);
+near_contract_standards::impl_fungible_token_storage!(Contract, token, on_account_closed);
+
+#[near_bindgen]
+impl FungibleTokenMetadataProvider for Contract {
+    fn ft_metadata(&self) -> FungibleTokenMetadata {
+        self.metadata.get().unwrap()
+    }
+}
